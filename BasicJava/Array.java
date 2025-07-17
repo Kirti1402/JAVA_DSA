ade3801833dd6d74e3f47186a0b1e50a6478749f
Kirti Singh <kirtisingh1497@gmail.com>
2025-07-17 22:39:03 +0530

    learned basic array and ifelse

---
+++ BasicJava/Array.java
@@ -0,0 +1,9 @@
+public class Array {
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5};
+        System.out.println(arr[4]);
+        //System.out.println(arr[7]);
+        String names[] ={"Kirti","Swati","Om"};
+        System.out.println(names[2]);
+    }
+}
