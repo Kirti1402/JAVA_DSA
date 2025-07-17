0ad50a945e678e6c3dc9c817f33257a5438073fa
Kirti Singh <kirtisingh1497@gmail.com>
2025-07-17 21:10:07 +0530

    Basic Java

---
+++ BasicJava/Sum.java
@@ -0,0 +1,10 @@
+class Sum {
+    public static void main(String[] args) {
+        System.out.println(sum(5,6));
+    }
+
+    public static int sum(int a,int b){
+        int sum = a + b;
+        return sum;
+    }
+}
