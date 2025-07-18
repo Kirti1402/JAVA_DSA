eb3f6bde0cd6885c1c6492f47836bc17faa6fb76
Kirti Singh <kirtisingh1497@gmail.com>
2025-07-18 14:06:59 +0530

    Right angled number and star pattern

--- BasicJava/StarPattern.java
+++ BasicJava/StarPattern.java
@@ -1,12 +1,33 @@
 public class StarPattern {
     public static void main(String[] args) {
-        starPatternOne(4, 4);
+        starPatternOne(4);
+        rightAngledPattern(6);
+        rightAngledNumberPattern(5);
     }
-    public static void starPatternOne(int n1,int n2){
+    /*************************************************************************/
+    public static void starPatternOne(int n1){
         System.out.println("Star Pattern One");
         for(int i=1;i<=n1;i++){
             for(int j=1;j<=n1;j++){
                 System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    /*************************************************************************/
+    public static void rightAngledPattern(int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+    /*************************************************************************/
+    public static void rightAngledNumberPattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(j+" ");
             }
             System.out.println("");
         }
