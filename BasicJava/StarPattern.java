5dff9379a47bdf6e8e5961f041dd2cd3032477e0
Kirti Singh <kirtisingh1497@gmail.com>
2025-07-22 21:19:12 +0530

    Right-Angled Triangle of Alternating 1s and 0s

--- BasicJava/StarPattern.java
+++ BasicJava/StarPattern.java
@@ -7,6 +7,7 @@ public class StarPattern {
         rightAngledNumberReversedPattern(5);
         rightAngledRightAlignedPattern(5);
         rightAngledRightAlignedPattern(0);
+        rightAngledTriangleofAlternating(6);
     }
     /*************************************************************************/
     public static void starPatternOne(int n1){
@@ -75,5 +76,23 @@ public class StarPattern {
             }
             System.out.println("");
         }
+    }
+
+    public static void rightAngledTriangleofAlternating(int n){
+        if(n<=0){
+            System.out.println("Number should be Greater than 0");
+            return;
+        }
+         System.out.println("Right-Angled Triangle of Alternating 1s and 0s *********************************");
+        
+         for(int i=1;i<=n;i++){
+            int k=0;
+            for(int j=1;j<=i;j++){
+                k = (k == 0) ? 1 : 0;
+                System.out.print(k+" ");
+            }
+           
+            System.out.println("");
+         }
     }
 }
