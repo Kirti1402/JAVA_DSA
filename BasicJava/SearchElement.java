4be4d14fafe1d4ec2008922834f0c76e599f37a4
Kirti Singh <kirtisingh1497@gmail.com>
2025-07-23 20:17:34 +0530

    format

--- BasicJava/SearchElement.java
+++ BasicJava/SearchElement.java
@@ -12,7 +12,6 @@ public class SearchElement {
         for(int i=0;i<arr.length;i++){
             if(arr[i] == num){
                  return "index:" + i;
-
             }
         }
         return "Not found -1";
